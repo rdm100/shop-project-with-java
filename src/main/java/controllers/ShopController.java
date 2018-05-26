ae6d53094096d2489e03faa44f310f762db329ed
Paul Kelly <paul.kelly20@gmail.com>
2018-05-26 11:17:29 +0100

    Added homepage

--- src/main/java/controllers/ShopController.java
+++ src/main/java/controllers/ShopController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import db.DBHelper;
 import db.Seeds;
+import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
 
 import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
@@ -14,9 +19,11 @@ public class ShopController {
 
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
-        get("/hello", (req, res) -> {
-            return "Hello, World!";
-        });
+        get("/shop", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/shop/index.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+    }
 
-
-    }}
+    }
